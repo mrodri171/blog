df3016ec46e39a8db3587ec18df7b4768873924b
rodriguez <mrodri17@unm.edu>
2016-10-27 16:33:54 -0600

    Format js not working

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ gem 'record_tag_helper', '~> 1.0'
 gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
+
+gem 'pry-rails'
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
 # Use SCSS for stylesheets
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  before_action :set_post
   before_action :set_comment, only: [:show, :edit, :update, :destroy]
 
   # GET /comments
@@ -24,13 +25,15 @@ class CommentsController < ApplicationController
   # POST /comments
   # POST /comments.json
   def create
-    @comment = Comment.new(comment_params)
+    @comment = @post.comments.new(comment_params)
+    #@comment = Comment.new(comment_params)
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
-        format.json { render :show, status: :created, location: @comment }
-        format.js
+        format.js{ render :nothing => true }
+        #binding.pry
+        #format.html { redirect_to post_path(@post.id), notice: 'Comment was successfully created.' }
+        #format.json { render :show, status: :created, location: @comment }
       else
         format.html { render :new }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
@@ -64,8 +67,13 @@ class CommentsController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
+    def set_post
+     @post = Post.find(params[:post_id])
+    end
+
     def set_comment
-      @comment = Comment.find(params[:id])
+      @comment = @post.comments.find(params[:id])
+      #@comment = Comment.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
